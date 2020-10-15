435d3ecdf876147d2705b470b2916f43896a9172
Soveu <marx.tomasz@gmail.com>
2020-10-15 12:24:55 +0200

    stuff

--- asm/boot.asm
+++ asm/boot.asm
@@ -10,6 +10,8 @@ dd 0              ; no flags
 dd -0x1BADB002    ; checksum
 
 _start:
+  mov eax, 0x2F692F48
+  mov [0xb8000], eax
   hlt
   jmp _start
 
--- build.rs
+++ build.rs
@@ -2,11 +2,16 @@ use std::process::Command;
 
 fn main() {
     println!("cargo:rerun-if-changed=asm/boot.asm");
-    println!("cargo:rerun-if-changed=asm/linker.ld");
+    println!("cargo:rerun-if-changed=asm/linker.asm");
 
-    Command::new("yasm")
+    println!("cargo:warning=Running yasm");
+    let status = Command::new("yasm")
         .args(&["-f", "elf64", "asm/boot.asm"])
         .status()
         .unwrap();
+
+    let code = status.code().unwrap();
+    println!("cargo:warning=yasm ended with code {}", code);
+    assert_eq!(code, 0);
 }
 
